cc38e5cfd4184dcfa956ae5c3e480d29c17ca72f
yangfan <yangfan@yangfandeMac-mini.local>
2026-08-04 16:41:13 +0800

    优化服务器二部署方式

--- backend/run.py
+++ backend/run.py
@@ -8,7 +8,7 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(
-        host="0.0.0.0",
+        host=os.environ.get("HOST", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=os.environ.get("FLASK_DEBUG", "0") == "1",
     )
